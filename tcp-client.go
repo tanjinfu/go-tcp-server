0dd3d29bc3843be5dcc4db51331bd8368162ac27
Tan Jinfu <tanjinfu@gmail.com>
2016-09-20 17:04:06 +0800

    Log elapse time.

--- tcp-client.go
+++ tcp-client.go
@@ -3,25 +3,27 @@ package main
         import "net"
         import "fmt"
         import "bufio"
-        import "os"
+        import "time"
+        import "strconv"
 
         func main() {
 
         // connect to this socket
-        // var address string = "127.0.0.1:31069"
+        var address string = "127.0.0.1:31069"
         // var address string = "10.74.113.215:1069"
-        var address string = "10.74.113.217:1069"
+        // var address string = "10.74.113.217:1069"
         // fmt.Println("Connected to "+address)
         conn, _ := net.Dial("tcp", address)
         for {
-        // read in input from stdin
-        reader := bufio.NewReader(os.Stdin)
-        fmt.Print("Text to send: ")
-        text, _ := reader.ReadString('\n')
+
+        text := "a"
         // send to socket
+        sendingTime := time.Now().Nanosecond()
         fmt.Fprintf(conn, text + "\n")
         // listen for reply
         message, _ := bufio.NewReader(conn).ReadString('\n')
-        fmt.Print("Message from server: "+message)
+        receivingTime := time.Now().Nanosecond();
+        fmt.Println("Message from server: "+message+ " time(Nanosecond): "+strconv.Itoa(receivingTime-sendingTime))
+        time.Sleep(time.Second*2)
         }
         }
